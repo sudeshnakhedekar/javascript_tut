448a3f792abbc55af95fbe94173a188b9f1cd199
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-01 13:14:37 +0530

    initial comment on javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+2 + 3;
+
+if(2+3===5){
+    console.log("Helloooo");
+}
+
+console.log("Helloooo");
+console.log("Helloooo");
+console.log("Helloooo");
+console.log("Helloooo");
+console.log("Helloooo");
