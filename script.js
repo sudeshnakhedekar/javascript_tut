196ac14636daba71d542db9ce848f987afca62ba
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-05 11:39:23 +0530

    datastructure obj in js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-var list=["tiger", "cat", "bear","birds"];
+var user={
+    name: "Aditya",
+    age: 24,
+    hobby: "Travelling",
+    isMarried: false,
+    spells:["abrakadra","shazam","boo"],
+    shout: function() {
+        console.log ("AHHHHHHH!");
+    }
+};
+
+
+var list =[ 
+
+    {
+        username: "andy",
+        password: "secre"
+    },
+    {
+        username: "jess",
+        password: "124"
+    }
+];
