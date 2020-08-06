d1dd87c88e21b7550f2acb8ed13233406b5381ff
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-06 11:53:31 +0530

    basic signin of facebook

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-var user={
-    name: "Aditya",
-    age: 24,
-    hobby: "Travelling",
-    isMarried: false,
-    spells:["abrakadra","shazam","boo"],
-    shout: function() {
-        console.log ("AHHHHHHH!");
-    }
-};
-
-
-var list =[ 
-
-    {
-        username: "andy",
-        password: "secre"
-    },
-    {
-        username: "jess",
-        password: "124"
-    }
+var database = [
+{
+    username: "sudeshna",
+    password: "supersecret"
+}    
 ];
+
+var newsFeed = [
+{
+    username: "Bobby",
+    timeline: "So tired from all that learning!"
+},
+{
+    username: "Sally",
+    timeline: "Javascript is so cool!"
+}
+];
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("What is your password?");
+
+function signIn(user, pass){
+    if (user=== database[0].username &&
+         pass === database[0].password) {
+             console.log(newsFeed);
+         }else{
+             alert("Sorry, wrong username and password!")
+         }
+}
+
+signIn(userNamePrompt, passwordPrompt);
