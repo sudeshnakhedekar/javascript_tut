8e7766e28f4aedb42aca5e5230332702c4ba6e41
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-07 18:06:18 +0530

    using do while  loop in js

--- script.js
+++ script.js
@@ -8,10 +8,17 @@ var todos =[
  var todosLength = todos.length;
 //for (var i=0; i< todosLength; i++) {
 // todos.pop();
-//} 
 
+//} 
 var counterOne= 10; 
-while (counterOne > 0) {
-    console.log(counterOne);
+while (counterOne >10) {
+    console.log("while",counterOne);
     counterOne--;
 }  
+
+
+var counterTwo =10
+do{
+    console.log("do while",counterTwo);
+    counterTwo--;
+}while (counterTwo >10);
