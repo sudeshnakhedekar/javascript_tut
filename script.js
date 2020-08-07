1358cdeb26cf4d8ed5a2ef226bca6a2198a6ff7b
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-07 18:25:32 +0530

    using foreach  loop in js

--- script.js
+++ script.js
@@ -5,20 +5,39 @@ var todos =[
     "study javascript",
     "eat healthy"
 ];
+ 
+var todosImportant =[
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
+];
  var todosLength = todos.length;
-//for (var i=0; i< todosLength; i++) {
-// todos.pop();
+// for (var i=0; i< todosLength; i++) {
+//  console.log(todos[i], i);
 
-//} 
-var counterOne= 10; 
-while (counterOne >10) {
-    console.log("while",counterOne);
-    counterOne--;
-}  
+// }
+function logTodos(todo, i){
+    console.log(todo, i);
+}
+ 
+todos.forEach(logTodos);
+todosImportant.forEach(logTodos);
 
 
-var counterTwo =10
-do{
-    console.log("do while",counterTwo);
-    counterTwo--;
-}while (counterTwo >10);
+
+
+
+// var counterOne= 10; 
+// while (counterOne >10) {
+    // console.log("while",counterOne);
+    // counterOne--;
+// }  
+
+
+// var counterTwo =10
+// do{
+    // console.log("do while",counterTwo);
+    // counterTwo--;
+// }while (counterTwo >10);
