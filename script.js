9d71a5f5f99475a6882bc5fe21b0632165fa3c86
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-07 17:57:10 +0530

    using while  loop in js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ var todos =[
     "eat healthy"
 ];
  var todosLength = todos.length;
-for (var i=0; i< todosLength; i++) {
- todos.pop();
-} 
+//for (var i=0; i< todosLength; i++) {
+// todos.pop();
+//} 
+
+var counterOne= 10; 
+while (counterOne > 0) {
+    console.log(counterOne);
+    counterOne--;
+}  
