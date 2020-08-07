6a45341f121052402524aca4715c6f0a75403e7f
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-07 17:48:57 +0530

    using for loop in js

--- script.js
+++ script.js
@@ -1,31 +1,11 @@
-var database = [
-{
-    username: "sudeshna",
-    password: "supersecret"
-}    
+var todos =[
+    "clean room",
+    "brush teeth",
+    "exercise",
+    "study javascript",
+    "eat healthy"
 ];
-
-var newsFeed = [
-{
-    username: "Bobby",
-    timeline: "So tired from all that learning!"
-},
-{
-    username: "Sally",
-    timeline: "Javascript is so cool!"
-}
-];
-
-var userNamePrompt = prompt("What is your username?");
-var passwordPrompt = prompt("What is your password?");
-
-function signIn(user, pass){
-    if (user=== database[0].username &&
-         pass === database[0].password) {
-             console.log(newsFeed);
-         }else{
-             alert("Sorry, wrong username and password!")
-         }
-}
-
-signIn(userNamePrompt, passwordPrompt);
+ var todosLength = todos.length;
+for (var i=0; i< todosLength; i++) {
+ todos.pop();
+} 
