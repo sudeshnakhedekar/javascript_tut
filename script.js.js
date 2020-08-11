1938320b4de38dc228c6808ff37f1d734ed5b4b8
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-11 22:53:25 +0530

    keypress and listing items using enter in js + dom

--- script.js.js
+++ script.js.js
@@ -1,7 +1,25 @@
-var button = document.getElementsByTagName("button")[0];
+var button = document.getElementById("enter");
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
+    if (input.value.length > 0 ){
+        var li= document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value ="";
+    }
+}) 
+
+input.addEventListener("keypress", function(event) {
+
+    if (input.value.length > 0 && event.which ===13){
+        var li= document.createElement("li");
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value ="";
+    }
+
+}) 
 
 
-    console.log("Click!!!!");
-})
