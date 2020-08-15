610daacf90ad0608e79206a62406ed90084ad10e
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-15 11:37:54 +0530

    ternary operator

---
+++ script2.js
@@ -0,0 +1,20 @@
+//  condition ? expr1 : expr2;
+
+
+function isUserValid(bool) {
+    return bool;
+}
+
+var answer = isUserValid(true) ? "You may enter" : "Access Denied";
+
+var automatedAnswer =
+"Your account # is " + (isUserValid(false) ? "1234" : "not available");
+
+
+
+
+
+
+
+
+//TERNARY OPERATOR//
