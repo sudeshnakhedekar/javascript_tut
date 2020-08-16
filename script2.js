8a4d91187e3a847762c53ab226047b0d2eb26220
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-16 12:06:35 +0530

    switch statement

--- script2.js
+++ script2.js
@@ -1,14 +1,25 @@
-//  condition ? expr1 : expr2;
+function moveCommand(direction){
+    var whatHappens;
+    switch(direction) {
+    case "forward":
+    whatHappens= "you encounter a monster";
+    break;
+    case "back":
+    whatHappens= "you arrived home";
+    break;
+    case "right":
+    whatHappens= "you found a river";
+    break;
+    case "left":
+    whatHappens= "you run into a troll ";
+    break;
+    default:
+        whatHappens ="please enter a valid direction";
 
-
-function isUserValid(bool) {
-    return bool;
+}
+return whatHappens;
 }
 
-var answer = isUserValid(true) ? "You may enter" : "Access Denied";
-
-var automatedAnswer =
-"Your account # is " + (isUserValid(false) ? "1234" : "not available");
 
 
 
@@ -16,5 +27,18 @@ var automatedAnswer =
 
 
 
+//Switch operator//
+//o/p
+// window.moveCommand("forward");
+// "you encounter a monster"
+// window.moveCommand("back");
+// "you arrived home"
+// window.moveCommand("right");
+// "you found a river"
+// window.moveCommand("left");
+// "you run into a troll "
+// window.moveCommand(111111);
+// "please enter a valid direction"//
 
-//TERNARY OPERATOR//
+
+
