6b5be7c7a3632d49630c6f26673de4f0759247d7
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-16 15:13:25 +0530

    const and let instead of vae

---
+++ script2.2.js
@@ -0,0 +1,19 @@
+// let + const
+
+const player = 'sobby';
+let experience = 100;
+let wizardLevel = false; 
+
+if  (experience > 90) {
+    let wizardLevel = true;
+}
+
+const obj ={
+    player: 'sobby',
+    experience: 100,
+    wizardLevel: false
+}
+
+
+
+//use const and let insted of var//
