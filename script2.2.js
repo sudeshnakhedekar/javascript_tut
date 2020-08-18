ad0bea4c0a99d9ab59814090bb9a85a7d0d83940
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-18 22:06:19 +0530

    symbol

--- script2.2.js
+++ script2.2.js
@@ -1,8 +1,22 @@
-// Object Properties
+//System//
 
-const name = 'john snow';
-const obj = {
-    [name]: 'hello',
-    [1+2]: 'hihi'
-    
-}
+let sym1 = Symbol();
+let sym2 = Symbol('foo');
+let sym3 = Symbol('fooo');
+let sym4 = Symbol('foooo');
+
+
+
+
+
+//o/p
+
+
+// sym1
+// Symbol()
+// sym2
+// Symbol(foo)
+// sym3
+// Symbol(fooo)
+// sym4
+// Symbol(foooo)
