aca7f673fa5537c597f5e9f0ee72e25ada1e40e5
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-18 21:53:52 +0530

    obj properties

--- script2.2.js
+++ script2.2.js
@@ -1,17 +1,8 @@
+// Object Properties
 
-
-const  obj ={
-    player: 'bobby',
-    experience: 100,
-    wizardLevel: false
+const name = 'john snow';
+const obj = {
+    [name]: 'hello',
+    [1+2]: 'hihi'
+    
 }
-
-const player =obj.player;
-const player =obj.experience;
-let wizardLevel= obj.wizardLevel
-
-
-// using Destructuring  //
-
-const {player,experience}= obj;
-let { wizardLevel } =obj;
