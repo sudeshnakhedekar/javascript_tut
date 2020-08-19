adef1ae22da0f46f9be3f30e2b4521f37589150e
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-19 23:17:24 +0530

    Template strings amd default agrs

--- script2.2.js
+++ script2.2.js
@@ -1,10 +1,14 @@
-//arrow functions
+//Template string//
+const name="Sally";
+const age = 23;
+const pet = "dog";
 
-function add(a,b) {
-    return a+b;
+// const greeting = "hello" + name + "you seem to be doing " + greeting
+
+const greetingBest = 'hello ${name} you seem to be ${age-10}. what a lovely ${pet} you have' ;
+
+// default agrument //
+ 
+function greet(name='', age = 29, pet="cat" ){
+    return 'hello ${name} you seem to be ${age-10}. what a lovely ${pet} you have';
 }
-
-
-
-//using arrow//
-const add2 =(a,b) => a+b;
