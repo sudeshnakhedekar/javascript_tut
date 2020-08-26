ea7ba9e50ca51cf11ff1133293dbeb5274cae129
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-08-26 16:46:54 +0530

    Currying , closure  in advance javascript

--- script2.2.js
+++ script2.2.js
@@ -1,14 +1,35 @@
-//Template string//
-const name="Sally";
-const age = 23;
-const pet = "dog";
-
-// const greeting = "hello" + name + "you seem to be doing " + greeting
-
-const greetingBest = 'hello ${name} you seem to be ${age-10}. what a lovely ${pet} you have' ;
-
-// default agrument //
+const first = () => {
+    const greet = 'Hi';
+   const second = () => {
+        alert (greet);
+    }
+    return second;
+}
  
-function greet(name='', age = 29, pet="cat" ){
-    return 'hello ${name} you seem to be ${age-10}. what a lovely ${pet} you have';
+const newFunc = first();
+newfunc();
+
+//Closure - a fun ran, the function executed . It's never going to executed again.
+//But its going to remember that there are reference to those variable 
+//so the child scope always has access to the parent scope.
+
+//Currying
+const mutiply =(a,b) => a * b;
+const curriedMultiply = (a) => (b) =>  a * b;
+curriedMultiply(3);
+
+//Compose 
+const compose = (f,g) => (a) => f(g(a));
+
+const sum =(num) => num + 1;
+
+compose(sum, sum) (5);
+
+
+//Avoiding Side Effects and functional purity.
+
+
+var a= 1;
+function b() {
+    a =2;
 }
