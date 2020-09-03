04d72d2c8904e35dd9cacfd22a28ba4642f75702
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-09-03 21:41:51 +0530

    adv array

--- script2.2.js
+++ script2.2.js
@@ -1,35 +1,47 @@
-const first = () => {
-    const greet = 'Hi';
-   const second = () => {
-        alert (greet);
-    }
-    return second;
-}
- 
-const newFunc = first();
-newfunc();
-
-//Closure - a fun ran, the function executed . It's never going to executed again.
-//But its going to remember that there are reference to those variable 
-//so the child scope always has access to the parent scope.
-
-//Currying
-const mutiply =(a,b) => a * b;
-const curriedMultiply = (a) => (b) =>  a * b;
-curriedMultiply(3);
-
-//Compose 
-const compose = (f,g) => (a) => f(g(a));
-
-const sum =(num) => num + 1;
-
-compose(sum, sum) (5);
+//1 code
 
 
-//Avoiding Side Effects and functional purity.
+// const array = [1,2,10, 16];
+
+// const double =[]
+// const newArray = array.forEach((num) =>{
+//    double.push(num *2);
+// })
+//  console.log(double);
+
+ //map, filter, reduce
+ //2 code
+
+//  const array = [1,2,10, 16];
+
+// const double =[]
+// const newArray = array.forEach((num) =>{
+//    double.push(num *2);
+// })
+//  console.log(double);
 
 
-var a= 1;
-function b() {
-    a =2;
-}
+//  const mapArray = array.map((num)=>{
+//      return num *2;
+//  });
+
+//   console.log(mapArray);
+
+
+//3 code
+
+
+const array = [1,2,10, 16];
+
+const double =[]
+const newArray = array.forEach((num) =>{
+   double.push(num *2);
+})
+ console.log('forEach',double);
+
+
+ const mapArray = array.map((num)=>{
+     return num *2;
+ });
+
+  console.log('map',mapArray);
