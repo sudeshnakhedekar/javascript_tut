40949a29831f87ec4357c880e485eb241696d788
sudeshnakhedekar <sudeshnakhedekar@gmail.com>
2020-09-05 12:34:20 +0530

    adv array include map, filter and reduce

--- script2.2.js
+++ script2.2.js
@@ -1,31 +1,39 @@
 //1 code
 
 
-// const array = [1,2,10, 16];
+const array = [1,2,10, 16];
 
-// const double =[]
-// const newArray = array.forEach((num) =>{
-//    double.push(num *2);
-// })
-//  console.log(double);
+const double =[]
+const newArray = array.forEach((num) =>{
+   double.push(num *2);
+})
+ console.log(double);
+    
+
+
+
 
  //map, filter, reduce
+
+
+
+
  //2 code
 
-//  const array = [1,2,10, 16];
+ const array = [1,2,10, 16];
 
-// const double =[]
-// const newArray = array.forEach((num) =>{
-//    double.push(num *2);
-// })
-//  console.log(double);
+const double =[]
+const newArray = array.forEach((num) =>{
+   double.push(num *2);
+})
+ console.log(double);
 
 
-//  const mapArray = array.map((num)=>{
-//      return num *2;
-//  });
+ const mapArray = array.map((num)=>{
+     return num *2;
+ });
 
-//   console.log(mapArray);
+  console.log(mapArray);
 
 
 //3 code
@@ -48,21 +56,80 @@ const newArray = array.forEach((num) =>{
 
 
 
-
-  //example for 3
-
-  
+//
+//
+//
+//
 const array = [2,4,6,8,10];
 
 const double =[]
-const newArray = array.forEach((num) =>{
+const newArray = array.forEach(num =>{
+   double.push(num *4);
+})
+ console.log('forEach',double);
+ 
+// array map
+
+ const mapArray = array.map(num => num *4);
+
+  console.log('map',mapArray);
+
+
+
+  //
+  //
+  //
+  //
+
+const array = [2,4,6,8,10];
+
+const double =[]
+const newArray = array.forEach(num =>{
    double.push(num *4);
 })
  console.log('forEach',double);
 
+// arrat map
 
- const mapArray = array.map((num)=>{
-     return num *4;
- });
+ const mapArray = array.map(num => num *4);
 
   console.log('map',mapArray);
+
+//array filter
+
+const filterArray = array.filter(num => num> 5);
+console.log('filter', filterArray);
+
+//
+//
+//
+//
+//
+
+const array = [2,4,6,8,11];
+
+const double =[]
+const newArray = array.forEach(num =>{
+   double.push(num *4);
+})
+ console.log('forEach',double);
+
+// arrat map
+
+ const mapArray = array.map(num => num *4);
+
+  console.log('map',mapArray);
+
+//array filter
+
+const filterArray = array.filter(num => num> 5);
+console.log('filter', filterArray);
+
+
+//reduce
+
+const reducrArray = array.reduce((accumulator,num)=> {
+    return accumulator + num;
+}, 0)
+
+console.log('reduce', reducrArray);
